f063a98eddf14fb4466ced009ddb15c6e3125f31
Nilai Vemula <nilaivemula@gmail.com>
2020-12-06 23:00:43 -0600

    re-set up heroku

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo “\
 [general]\n\
-email = \”your-email@domain.com\”\n\
+email = \”nilai.r.vemula@vanderbilt.edu\”\n\
 “ > ~/.streamlit/credentials.toml
 echo “\
 [server]\n\
